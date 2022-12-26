42df7029133af43d763d23519894738cec6386dd
sarah-bouadi <sarahbouadi0508@gmail.com>
2022-12-26 22:55:24 +0100

    Adding signIn method for authentication

--- app/src/main/java/com/esgi/groupe9/android_frontend/EmailPasswordActivity.kt
+++ app/src/main/java/com/esgi/groupe9/android_frontend/EmailPasswordActivity.kt
@@ -50,6 +50,24 @@ class EmailPasswordActivity : Activity() {
 
     }
 
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
 
     private fun updateUI(user: FirebaseUser?) {
         // TODO : implement the logic for the user who were connected
