70433f47486e1f3758d62c391ca7b9e6e52b335f
sarah-bouadi <sarahbouadi0508@gmail.com>
2022-12-26 22:59:47 +0100

    Adding signOut for disconnect the user

--- app/src/main/java/com/esgi/groupe9/android_frontend/MainActivity.kt
+++ app/src/main/java/com/esgi/groupe9/android_frontend/MainActivity.kt
@@ -2,10 +2,17 @@ package com.esgi.groupe9.android_frontend
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Method to disconnect the user
+    private fun signOut() {
+        Firebase.auth.signOut()
     }
 }
