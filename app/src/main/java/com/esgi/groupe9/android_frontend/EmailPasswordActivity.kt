dd30bb60e3d4ca7795ecc22fd5feb3bef374776b
sarah-bouadi <sarahbouadi0508@gmail.com>
2022-12-26 22:44:22 +0100

    Adding createAccount and updateUI methods for authentication

--- app/src/main/java/com/esgi/groupe9/android_frontend/EmailPasswordActivity.kt
+++ app/src/main/java/com/esgi/groupe9/android_frontend/EmailPasswordActivity.kt
@@ -1,8 +1,12 @@
 package com.esgi.groupe9.android_frontend
 
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -24,6 +28,32 @@ class EmailPasswordActivity : Activity() {
         if (currentUser != null) {
             reload();
         }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+            }
+
+    }
+
+
+    private fun updateUI(user: FirebaseUser?) {
+        // TODO : implement the logic for the user who were connected
     }
 
     private fun reload() {
