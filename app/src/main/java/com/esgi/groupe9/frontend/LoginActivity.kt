ea99684462d35c91055c8c48d784dd8c1168bce3
[Sacha MOUCHON] <[sacha.mouchon@gmail.com]>
2023-01-02 23:17:16 +0100

    refactor code

--- app/src/main/java/com/esgi/groupe9/frontend/LoginActivity.kt
+++ app/src/main/java/com/esgi/groupe9/frontend/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,33 +14,62 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        logUser()
-        goRegister()
+        signInUser()
+        goOnRegisterPage()
     }
 
-    private fun logUser() {
+    private fun signInUser() {
         val loginButton = findViewById<Button>(R.id.login_button)
         loginButton.setOnClickListener {
-            val email = findViewById<EditText>(R.id.email_text)
-                .text
-                .toString()
-            val password = findViewById<EditText>(R.id.password_text)
-                .text
-                .toString()
+            val email = findViewById<EditText>(R.id.email_input_login).text.toString()
+            val password = findViewById<EditText>(R.id.password_input_login).text.toString()
+
+            when {
+                email.isEmpty() && password.isNotEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill the email input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+                password.isEmpty() && email.isNotEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill the password input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+                email.isEmpty() && password.isEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill both input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+            }
 
             Log.d("LoginActivity", "Attempt login with Email : $email")
-
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener {
-                    // TODO : check the successfully of this
+                    // TODO : check the successfully of this, on the next video of kotlin messenger
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    Log.d(
+                        "LoginActivity",
+                        "Successfully login user with the UserID : ${it.result.user?.uid}"
+                    )
                 }
-
-            // TODO : add addOnFailureListener()
+                // TODO : add addOnFailureListener(), on the next video of kotlin messenger
+                .addOnFailureListener {
+                    Log.d("LoginActivity", "Failed to login user due to : ${it.message}")
+                    Toast.makeText(
+                        this,
+                        "Failed to login user due to : ${it.message}",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
         }
     }
 
-    private fun goRegister() {
-        val registerButton = findViewById<Button>(R.id.signup_button)
+    private fun goOnRegisterPage() {
+        val registerButton = findViewById<Button>(R.id.register_button)
         registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/esgi/groupe9/frontend/RegisterActivity.kt
+++ app/src/main/java/com/esgi/groupe9/frontend/RegisterActivity.kt
@@ -13,48 +13,42 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
 
-        registerUser()
-        goBack()
+        signUpUser()
+        goOnLoginPage()
     }
 
-    private fun registerUser() {
-        val registerButton = findViewById<Button>(R.id.signup_button_register)
+    private fun signUpUser() {
+        val registerButton = findViewById<Button>(R.id.create_account_button)
         registerButton.setOnClickListener {
-            val username = findViewById<EditText>(R.id.username_text_register)
-                .text
-                .toString()
-            val email = findViewById<EditText>(R.id.email_text_register)
-                .text
-                .toString()
-            val password = findViewById<EditText>(R.id.password_text_register)
-                .text
-                .toString()
-            val confirmPassword = findViewById<EditText>(R.id.confirm_password_text_register)
-                .text
-                .toString()
+            val username = findViewById<EditText>(R.id.username_input_register).text.toString()
+            val email = findViewById<EditText>(R.id.email_input_register).text.toString()
+            val password = findViewById<EditText>(R.id.password_input_register).text.toString()
+            val confirmPassword = findViewById<EditText>(R.id.confirm_password_input_register).text.toString()
 
-            // TODO : try to refactor this also and use switch case
-            if (email.isEmpty() && password.isNotEmpty()) {
-                Toast
-                    .makeText(this, "Please fill the email input text", Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
-            }
-            if (password.isEmpty() && email.isNotEmpty()) {
-                Toast
-                    .makeText(this, "Please fill the password input text", Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
-            }
-            if (email.isEmpty() && password.isEmpty()) {
-                Toast
-                    .makeText(this, "Please fill both input text", Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
+            when {
+                email.isEmpty() && password.isNotEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill the email input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+                password.isEmpty() && email.isNotEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill the password input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+                email.isEmpty() && password.isEmpty() -> {
+                    Toast
+                        .makeText(this, "Please fill both input text", Toast.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
             }
 
-            /* TODO : implement logic to check the equality between password and
-                confirmPassword if it's not, set the colorText to red and set the create button to disable.
+            /* TODO :
+                implement logic to check the equality between password and confirm Password
+                if it's not, set the colorText to red and set the create button to disable.
             */
 
             Log.d("RegisterActivity", "Try to register a User to the Firebase Authentication")
@@ -68,19 +62,18 @@ class RegisterActivity : AppCompatActivity() {
                 }
                 .addOnFailureListener {
                     Log.d("RegisterActivity", "Failed to create user due to : ${it.message}")
-                    Toast
-                        .makeText(
-                            this,
-                            "Failed to create user due to : ${it.message}",
-                            Toast.LENGTH_SHORT
-                        )
+                    Toast.makeText(
+                        this,
+                        "Failed to create user due to : ${it.message}",
+                        Toast.LENGTH_SHORT
+                    )
                         .show()
                 }
         }
     }
 
-    private fun goBack() {
-        val backButton = findViewById<Button>(R.id.return_login_button)
+    private fun goOnLoginPage() {
+        val backButton = findViewById<Button>(R.id.return_login_page)
         backButton.setOnClickListener {
             finish()
         }
