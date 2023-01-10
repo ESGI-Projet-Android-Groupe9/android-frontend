7c5f42936ef9128f373055f51ba39e368e36b573
sarah-bouadi <sarahbouadi0508@gmail.com>
2023-01-10 23:02:10 +0100

    Adding the home page layout

--- app/src/main/java/com/esgi/groupe9/frontend/HomeActivity.kt
+++ app/src/main/java/com/esgi/groupe9/frontend/HomeActivity.kt
@@ -2,11 +2,42 @@ package com.esgi.groupe9.frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.esgi.groupe9.frontend.entity.Game
+import com.esgi.groupe9.frontend.entity.generateFakeGame
+import com.esgi.groupe9.frontend.entity.generateFakeTitanFallGame
+import com.esgi.groupe9.frontend.utils.GameListAdapter
+import com.esgi.groupe9.frontend.utils.OnGameListener
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val games = listOf(generateFakeTitanFallGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame(),
+            generateFakeGame()
+        )
+
         setContentView(R.layout.activity_home)
+
+        findViewById<RecyclerView>(R.id.games_list_view_home).apply {
+            layoutManager = LinearLayoutManager(this@HomeActivity)
+            adapter = GameListAdapter(games, object : OnGameListener {
+                override fun onClicked(game: Game, position: Int) {
+                    Toast.makeText(this@HomeActivity, "Game $position clicked", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 }
---
+++ app/src/main/java/com/esgi/groupe9/frontend/utils/GameListViewer.kt
@@ -0,0 +1,56 @@
+package com.esgi.groupe9.frontend.utils
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.esgi.groupe9.frontend.R
+import com.esgi.groupe9.frontend.entity.Game
+
+class GameListAdapter(
+    private val games: List<Game>,
+    private val listener: OnGameListener,
+) : RecyclerView.Adapter<GameViewHolder>() {
+
+    override fun getItemCount(): Int = games.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        return GameViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.game_item_preview_home, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        val game = games[position]
+        holder.updateGame(game)
+
+        holder.itemView.setOnClickListener {
+            listener.onClicked(game, position)
+        }
+    }
+}
+
+class GameViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    private val gameName = v.findViewById<TextView>(R.id.game_name_item_list)
+    private val gameEditor = v.findViewById<TextView>(R.id.game_editor_item_list)
+    private val gamePrice = v.findViewById<TextView>(R.id.game_price_item_list)
+    private val gameImage = v.findViewById<ImageView>(R.id.game_image_item_list)
+    private val gameBackgroundImage = v.findViewById<ImageView>(R.id.game_background_item_list)
+
+    fun updateGame(game: Game) {
+        gameName.text = game.name
+        gameEditor.text = game.editor
+        gamePrice.text = "Prix: " + game.price + " €"//TODO to generify
+//        gameImage.setImageBitmap(getImageBitmap(game.thumbnail))
+//        gameBackgroundImage.setImageBitmap(getImageBitmap(game.backgroundRaw))
+    }
+}
+
+interface OnGameListener {
+    fun onClicked(game: Game, position: Int)
+}
